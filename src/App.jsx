2ca4ad3dadd7d13941a42e7b7347efa32476aa82
nivasreddy2400 <nivasreddy2401@gmail.com>
2023-05-19 11:09:34 +0530

    First

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,67 @@ import photo1 from './assets/photo1.png'
 const App = () => {
   return (
     <div className="relative w-1920 h-auto mx-auto bg-white shadow-cst rounded-lg">
+      <div className="font-bold font-mono text-3xl absolute w-278 h-47 top-110 left-243">
+        The creative crew
+      </div>
+      <div className="absolute w-44 h-7 top-110 left-736 uppercase font-semibold font-sans text-2xl">
+        Who we are
+      </div>
+      <div className='absolute top-147 left-736 w-439 text-2xl font-thin'>
+        We are team of creatively diverse. driven. innovative individuals
+        working in various locations from the world.
+      </div>
       <div className="absolute w-237 h-341 left-60 top-80">
-        <img src={photo1} />
+        <figure>
+          <img src={photo1} alt="" />
+          <figcaption className="font-semibold">
+            Bill Mahoney <br /> Product Owner
+          </figcaption>
+        </figure>
       </div>
       <div className="absolute w-237 h-341 left-571 top-429">
-        <img src={photo2} />
+        <figure>
+          <img src={photo2} alt="" />
+          <figcaption className="font-semibold">
+            Saba Cabrera <br /> Art Director
+          </figcaption>
+        </figure>
       </div>
       <div className="absolute w-237 h-341 left-902 top-330">
-        <img src={photo3} />
+        <figure>
+          <img src={photo3} alt="" />
+          <figcaption className="font-semibold">
+            Shae Le <br />
+            Tech Lead
+          </figcaption>
+        </figure>
       </div>
       <div className="absolute w-237 h-341 left-241 top-845">
-        <img src={photo4} />
+        <figure>
+          <img src={photo4} alt="" />
+          <figcaption className="font-semibold">
+            Skylah Lu <br />
+            UX Designer
+          </figcaption>
+        </figure>
       </div>
       <div className="absolute w-237 h-341 left-571 top-944">
-        <img src={photo5} />
+        <figure>
+          <img src={photo5} alt="" />
+          <figcaption className="font-semibold">
+            Griff Richards <br />
+            Developer
+          </figcaption>
+        </figure>
       </div>
       <div className="absolute w-237 h-341 left-902 top-845">
-        <figure src={photo6} className="absolute w-335 h-449 left-865 top-796"></figure>
+        <figure>
+          <img src={photo6} alt="" />
+          <figcaption className="font-semibold">
+            Stan John <br />
+            Developer
+          </figcaption>
+        </figure>
       </div>
       <div className="absolute w-400 h-17 left-547 top-1448 ">
         created by NivasReddyP - devChallenges.io
